db07a98f7585b300979dfc2d4a080b616ab3bb3b
Nick <przebienda.n@gmail.com>
2016-04-19 03:42:22 -0400

    commit

--- app/Patientvisit.php
+++ app/Patientvisit.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Patientvisit extends Model
 {
-    //
+    public function Room()
+    {
+    	return $this->belongsTo('App\Room');
+    }
+
+    public function Patient()
+    {
+    	return $this->belongsTo('App\Patient'); 
+    }
+
+    public function Referral()
+    {
+    	return $this->belongsTo('App\Referral'); 
+    }
+
+    public function Service()
+    {
+    	return $this->hasMany('App\Service'); 
+    }
+ 
+    public function Procedurenote()
+    {
+    	return $this->hasMany('App\Procedurenote');
+    }
+
+    public function Test()
+    {
+    	return $this->belongsTo('App\Test');
+    }
+    
+    public function Diagnosisnote()
+    {
+    	return $this->hasMany('App\Diagnosisnote'); 
+    }
 }
