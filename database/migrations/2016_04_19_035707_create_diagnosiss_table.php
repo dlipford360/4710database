6369868232d9af4dd51cad89bd16013fce2df823
Daniel Lipford <d.l.lipford@hotmail.com>
2016-04-18 23:59:12 -0400

    create tables

---
+++ database/migrations/2016_04_19_035707_create_diagnosiss_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDiagnosissTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diagnosiss', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('diagnosiss');
+    }
+}
